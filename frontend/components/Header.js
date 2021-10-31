ca00caa466934619c1b04625dc165ace1dd1b176
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-10-31 08:06:00 -0400

    add styles to header and nav

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -24,7 +24,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -41,8 +41,8 @@ export default function Header() {
         <Logo>
           <Link href="/">Sick fits</Link>
         </Logo>
+        <Nav />
       </div>
-      <Nav />
       <div className="sub-bar">
         <p>Search</p>
       </div>
