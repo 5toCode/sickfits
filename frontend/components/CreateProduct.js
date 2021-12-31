ebe5074d1e4926b478f869887aa41e7aa0731fab
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-12-31 12:08:01 -0500

    add mutation for GQL createproduct

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,20 +1,71 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 
 import Form from './styles/Form';
 
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are getting passed in, and what types are they?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
+
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
     name: 'Shoes',
     price: 3234,
     description: 'These are the best shoes!',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
-    <Form>
-      <fieldset>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        console.log(inputs);
+        // submit the input fields to the backend
+        await createProduct();
+        clearForm();
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
-          <input type="file" id="name" name="image" onChange={handleChange} />
+          <input
+            required
+            type="file"
+            id="name"
+            name="image"
+            onChange={handleChange}
+          />
         </label>
         <label htmlFor="name">
           Name
@@ -38,6 +89,17 @@ export default function CreateProduct() {
             onChange={handleChange}
           />
         </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+
         <button type="submit">+ Add Product</button>
       </fieldset>
     </Form>
